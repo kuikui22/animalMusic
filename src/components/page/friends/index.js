6640c955914c312721b80a25f99313b63f5c86bd
basicBess <xinhua.bess@gmail.com>
2019-07-30 13:40:52 +0800

    新增'朋友'頁面

---
+++ src/components/page/friends/index.js
@@ -0,0 +1,109 @@
+import navMenu from '_components/menu/index.vue';
+
+export default {
+    data() {
+        return {
+            friendsActionList: []
+        }
+    },
+    components: {
+        navMenu
+    },
+    methods: {
+        getFriendActionList() {
+            this.friendsActionList = [
+                { 
+                    ID: '0',
+                    friends: {
+                        friendName: 'friend_0', 
+                        friendImg: '/images/gray-wolf_p1_1.jpg', 
+                        fans: '10.2萬'
+                    },          
+                    article: {
+                        action: '分享單曲',
+                        plays: '123456', 
+                        comments: '45678', 
+                        great: '78945',
+                        img: '/images/gray-wolf.jpg',
+                        content: '<html>..... <a link.....>',
+                    },
+                    images: [],
+                    musicList: {
+                        music_name: 'gray-wolf',
+                        music_author: 'GRAY-WOLF',
+                        music_img: '/images/gray-wolf_p1_1.jpg'
+                    }                    
+                },
+                { 
+                    ID: '1',
+                    friends: {
+                        friendName: 'friend_1', 
+                        friendImg: '/images/tiger_p1_1.jpg', 
+                        fans: '10.2萬'
+                    },          
+                    article: {
+                        action: '分享單曲',
+                        plays: '123456', 
+                        comments: '45678', 
+                        great: '78945',
+                        img: '/images/tiger.jpg',
+                        content: '<html>..... <a link.....>',
+                    },
+                    images: [],
+                    musicList: {
+                        music_name: 'gray-wolf',
+                        music_author: 'GRAY-WOLF',
+                        music_img: '/images/tiger_p1_1.jpg'
+                    }                    
+                },
+                { 
+                    ID: '2',
+                    friends: {
+                        friendName: 'friend_2', 
+                        friendImg: '/images/owl_p1_1.jpg', 
+                        fans: '10.2萬'
+                    },          
+                    article: {
+                        action: '分享單曲',
+                        plays: '123456', 
+                        comments: '45678', 
+                        great: '78945',
+                        img: '/images/owl.jpg',
+                        content: '<html>..... <a link.....>',
+                    },
+                    images: [],
+                    musicList: {
+                        music_name: 'owl',
+                        music_author: 'OWL',
+                        music_img: '/images/owl_p1_1.jpg'
+                    }                    
+                },
+                { 
+                    ID: '3',
+                    friends: {
+                        friendName: 'friend_3', 
+                        friendImg: '/images/fox_p1_1.jpg', 
+                        fans: '10.2萬'
+                    },          
+                    article: {
+                        action: '分享單曲',
+                        plays: '123456', 
+                        comments: '45678', 
+                        great: '78945',
+                        img: '/images/fox.jpg',
+                        content: '<html>..... <a link.....>',
+                    },
+                    images: [],
+                    musicList: {
+                        music_name: 'fox',
+                        music_author: 'GRAY-WOLF',
+                        music_img: '/images/fox_p1_1.jpg'
+                    }                    
+                },
+            ]; 
+        }
+    },
+    created() {
+        this.getFriendActionList();
+    }
+}
--- src/components/page/friends/index.vue
+++ src/components/page/friends/index.vue
@@ -1,6 +1,49 @@
 <template>
-    <div>
+    <div class="friends">
         <nav-menu></nav-menu>
-        friends
+        <div class="friends-contain">
+            <div class="friends-block" v-for="(item, index) in friendsActionList" :key="item.ID" :class="{'friends-block_border': index < (friendsActionList.length-1)}">
+                <div class="title media">
+                    <img :src="item.friends.friendImg" class="align-self-center" />
+                    <div class="media-body">
+                        <span class="name">{{ item.friends.friendName }}</span>
+                        {{ item.article.action }}:
+                        <small>{{ item.friends.fans }}</small>
+                        <span class="attention">+ 關注</span>
+                    </div>
+                </div>
+                <div class="content">
+                    {{ item.article.content }}
+                    <img :src="item.article.img" class="align-self-center" />
+                </div>
+                <div class="music" v-if="item.musicList.music_name">
+                    <div class="media">
+                        <img :src="item.friends.friendImg" class="align-self-center" />
+                        <div class="media-body">
+                            {{ item.musicList.music_name }}
+                            <small>{{ item.friends.fans }}</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom_info">
+                    <div>
+                        <font-awesome-icon icon="retweet" /> {{ item.article.plays }}
+                    </div>
+                    <div>
+                        <font-awesome-icon icon="comment-dots" /> {{ item.article.comments }}
+                    </div>
+                    <div>
+                        <font-awesome-icon icon="thumbs-up" /> {{ item.article.great }}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script type="text/javascript" src="./index.js"></script>
+
+<style lang="less" scoped>
+  @import url('~_assets/style/mixin.less');
+  @import url('./index.less');
+</style>
